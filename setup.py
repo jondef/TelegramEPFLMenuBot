ed61bbd2a907c59e252a6301979b3a3141d5aafa
jon <jdefilla@gmail.com>
2020-03-11 21:55:03 +0100

    Tried to implement an error handler

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="TelegramEPFLMenuBot",  # Replace with your own username
+	name="TelegramEPFLMenuBot",
 	version="0.0.1",
 	author="Jon Defilla",
 	author_email="jdefilla@gmail.com",
--- src/menu_epfl.py
+++ src/menu_epfl.py
@@ -1,8 +1,10 @@
 import datetime
 import json
+import logging
 import os
 import subprocess
 import sys
+import traceback
 import urllib.parse
 from datetime import timedelta
 from functools import wraps
@@ -18,7 +20,9 @@ from PIL import Image
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.utils.helpers import mention_html
 # pip install timeloop
 from timeloop import Timeloop
 
@@ -75,6 +79,7 @@ def init_bot():
 
 	BOT_TOKEN = get_bot_token()
 	UPDATER = Updater(token=BOT_TOKEN, use_context=True)
+	UPDATER.dispatcher.add_error_handler(error)
 
 
 #################################################
@@ -124,6 +129,58 @@ def get_menu_website():
 # link.click()
 
 # switch to iframe driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
+
+
+def start_logger():
+	logger = logging.getLogger(__name__)
+	ch = logging.StreamHandler(sys.stdout)
+	ch.setLevel(logging.INFO)
+	formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+	ch.setFormatter(formatter)
+	logger.addHandler(ch)
+	logger.setLevel(logging.INFO)
+	return logger
+
+
+# this is a general error handler function. If you need more information about specific type of update, add it to the
+# payload in the respective if clause
+def error(update, context):
+	# add all the dev user_ids in this list. You can also add ids of channels or groups.
+	devs = [1064553139]
+	# we want to notify the user of this problem. This will always work, but not notify users if the update is an
+	# callback or inline query, or a poll update. In case you want this, keep in mind that sending the message
+	# could fail
+	if update.effective_message:
+		text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
+		       "My developer(s) will be notified."
+		update.effective_message.reply_text(text)
+	# This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
+	# third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
+	# for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
+	# empty string works fine.
+	trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+	# lets try to get as much information from the telegram update as possible
+	payload = ""
+	# normally, we always have an user. If not, its either a channel or a poll update.
+	if update.effective_user:
+		payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+	# there are more situations when you don't get a chat
+	if update.effective_chat:
+		payload += f' within the chat <i>{update.effective_chat.title}</i>'
+		if update.effective_chat.username:
+			payload += f' (@{update.effective_chat.username})'
+	# but only one where you have an empty payload by now: A poll (buuuh)
+	if update.poll:
+		payload += f' with the poll id {update.poll.id}.'
+	# lets put this in a "well" formatted text
+	text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+	       f"</code>"
+	# and send it to the dev(s)
+	for dev_id in devs:
+		context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
+	# we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
+	raise
+
 
 def driver_click(driver, element):
 	driver.execute_script("arguments[0].click();", element)
@@ -352,6 +409,8 @@ def help_handler(update, context):
 @send_action(telegram.ChatAction.TYPING)
 @my_add_handler(CommandHandler("menu", lambda *args, **kwargs: menu_handler(*args, **kwargs)))
 def menu_handler(update, context):
+	lik = [1, 2, 3]
+	print(lik[5])
 	menu(update.effective_chat.id, context.bot)
 
 
@@ -421,17 +480,24 @@ def reset_handler(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, text="not implemented")
 
 
-
 #############################
 
 @my_add_handler(CommandHandler("update", lambda *args, **kwargs: update_handler(*args, **kwargs)))
 def update_handler(update, context):
-	# todo: check if user is server admin
+	# todo: check if user is server admin using handler user filer
 	# todo: this function is supposed to update the bot script
+	print("yep")
 	script_name = os.path.basename(__file__)  # with extension
-	string = f"import subprocess,sys,time,os;time.sleep(5);os.system('git pull');f=open('pulled.txt','w');f.close();time.sleep(3);subprocess.Popen([sys.executable, '{script_name}'],shell=False,stdout=None, stderr=None);f=open('done.txt','w');f.close()"
-	subprocess.Popen([sys.executable, "-c", string], shell=False, stdout=None, stderr=None)
+	print("damn")
+	string = f"import subprocess,sys,time,os;time.sleep(2);os.system('git pull');time.sleep(1);subprocess.Popen([sys.executable,'{script_name}'],shell=False,stdout=None, stderr=None)"
+	# os.execl(sys.executable, sys.executable, *sys.argv)
+	LOGGER.info(string)
 	context.bot.send_message(chat_id=update.effective_chat.id, text="Updating...")
+	subprocess.Popen([sys.executable, "-c", "import time;time.sleep(10);f=open('pulled.txt','w');f.close()"],
+	                 shell=False, stdout=None, stderr=None)
+	# subprocess.Popen([sys.executable, "-c", string], shell=False, stdout=None, stderr=None)
+
+	UPDATER.stop()
 	exit(0)
 
 
@@ -608,7 +674,9 @@ def auto_send_menu():
 
 
 def main():
-	global JSON_DATA, DRIVER
+	global JSON_DATA, DRIVER, LOGGER
+
+	LOGGER = start_logger()
 
 	init_bot()
 
